3f726c2ebd5475a8c8809c53a357d7c6d2031dfe
Rohan R Raikar <rohanraikar7993@gmail.com>
2025-11-10 14:37:48 +0530

    Add AuthApiController

---
+++ IdentityService/IdentityService/Controllers/ApiControllers/AuthApiController.cs
@@ -0,0 +1,32 @@
+﻿using IdentityService.Models.DTOs;
+using IdentityService.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthApiController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthApiController(IAuthService auth)
+        {
+            _authService = auth;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var token = await _authService.RegisterAsync(registerDto);
+            return Ok (token);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var token = await _authService.LoginAsync(dto);
+            return Ok(token);
+        }
+    }
+}
