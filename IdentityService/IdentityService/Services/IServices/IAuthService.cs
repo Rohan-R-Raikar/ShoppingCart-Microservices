223ff3d49270f295bf4b9c2242dc671d45186ceb
Rohan R Raikar <rohanraikar7993@gmail.com>
2025-11-10 14:31:03 +0530

    Add AuthService and IAuthService

---
+++ IdentityService/IdentityService/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using IdentityService.Models.DTOs;
+
+namespace IdentityService.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<TokenDto> LoginAsync(LoginDto dto);
+        Task<TokenDto> RegisterAsync(RegisterDto dto);
+    }
+}
