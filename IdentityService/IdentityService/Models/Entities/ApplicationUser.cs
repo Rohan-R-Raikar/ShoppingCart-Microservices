37c0abdc804e8d1fdcf81714166ae21c4029e73d
Rohan R Raikar <rohanraikar7993@gmail.com>
2025-11-10 12:24:51 +0530

    Initial Commit

---
+++ IdentityService/IdentityService/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Optional additional attributes
+        public string FullName { get; set; }
+    }
+}
